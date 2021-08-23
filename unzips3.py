38cff0f6dc0fc3bd07c266972c04d28829823ed9
Nick Airey <nairey@deloitte.com.au>
2021-08-23 12:59:09 +1000

    simplify returned file list

--- unzips3.py
+++ unzips3.py
@@ -2,23 +2,18 @@ from zipfile import ZipFile
 import boto3
 from io import BytesIO
 import json
-from datetime import datetime
-import time
 
 def lambda_handler(event, context):
 
-    def processZipObj(zipObj):
+    def extractAndUpload(zipObj):
         filename = zipObj.filename
-        s3.meta.client.upload_fileobj(zipfile.open(filename), Bucket=bucketName, Key=f'{filename}')
-        return {
-            "mediaUri": "s3://"+bucketName+"/"+filename,
-            "date": str(datetime.fromtimestamp(time.mktime(zipObj.date_time + (0,0,0))))
-        }
+        # s3.meta.client.upload_fileobj(zipfile.open(filename), Bucket=bucketName, Key=f'{filename}')
+        return "s3://"+bucketName+"/"+filename
 
     def isNotDir(zipObj): 
         return not zipObj.is_dir()
     
-    print(event)
+    # print(event)
     bucketName = event["bucketName"]
     keyInBucket = event["keyInBucket"]  
     s3 = boto3.resource('s3')
@@ -27,7 +22,7 @@ def lambda_handler(event, context):
     zipfile = ZipFile(BytesIO(s3zipObj.get()["Body"].read()), 'r')
 
     return {
-        "audioFiles": list(map(processZipObj, filter(isNotDir, zipfile.infolist())))
+        "files": list(map(extractAndUpload, filter(isNotDir, zipfile.infolist())))
     }
 
 print(json.dumps(lambda_handler(
